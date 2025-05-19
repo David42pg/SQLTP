5eab860c5e505d84b167bf1dd3e74247f78cf05a
David42pg <theomalogi@gmail.com>
2025-05-19 15:11:51 +0200

    Update CREATE TABLE.sql

--- modelisation/CREATE TABLE.sql
+++ modelisation/CREATE TABLE.sql
@@ -1,26 +1,23 @@
-CREATE TABLE utilisateur(
-   id_utilisateur VARCHAR(50),
+CREATE TABLE utilisateur (
+   id_utilisateur SERIAL PRIMARY KEY,
    nom VARCHAR(50),
    prenom VARCHAR(50),
-   email VARCHAR(50),
-   PRIMARY KEY(id_utilisateur)
+   email VARCHAR(100)
 );
 
-CREATE TABLE materiel(
-   id_materiel COUNTER,
+CREATE TABLE materiel (
+   id_materiel SERIAL PRIMARY KEY,
    nom VARCHAR(50),
    categorie VARCHAR(50),
-   quantite VARCHAR(50),
-   PRIMARY KEY(id_materiel)
+   quantite INTEGER
 );
 
-CREATE TABLE reservation(
-   id_reservation COUNTER,
-   date_debut VARCHAR(50),
-   date_fin VARCHAR(50),
-   id_materiel INT,
-   id_utilisateur VARCHAR(50),
-   PRIMARY KEY(id_reservation),
-   FOREIGN KEY(id_materiel) REFERENCES materiel(id_materiel),
-   FOREIGN KEY(id_utilisateur) REFERENCES utilisateur(id_utilisateur)
+CREATE TABLE reservation (
+   id_reservation SERIAL PRIMARY KEY,
+   date_debut DATE,
+   date_fin DATE,
+   id_materiel INTEGER,
+   id_utilisateur INTEGER,
+   FOREIGN KEY (id_materiel) REFERENCES materiel(id_materiel),
+   FOREIGN KEY (id_utilisateur) REFERENCES utilisateur(id_utilisateur)
 );
